08233a6d7f826da1b8d1e89fbc308977197b5ab5
romanfomin <rofominucvt@mail.ru>
2018-03-28 22:10:54 +0300

    fork + send START

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 all:
-	clang-3.5 -std=c99 -Wall -pedantic -o pa1 *.c
+	clang-3.5 -std=c99 -Wall -pedantic -o pa1 pa1.c
--- pa1.c
+++ pa1.c
@@ -6,51 +6,66 @@
 #include <stdio.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <time.h>
 
 #include "common.h"
 #include "ipc.h"
 #include "pa1.h"
 
-int close_fd(int fd);
-int close_unneccessary_fd(int** fd, int N, int local_proc_id);
-int fd_is_valid(int fd);
 int get_arg(int argc, char** argv);
-int fill_fd_matrix(int** fd_w, int** fd_r, int N);
-int** create_matrix(int N);
-int swap_parts(int** fd_w, int** fd_r, int N);
-int create_pipe(int** fd, int i, int j, int pfd[2],int log_fd);
+int*** create_matrix(int N);
+int fill_matrix(int*** matrix, int N);
+int close_unneccessary_fd(int*** matrix, int N, int proc_numb);
+int close_fd(int fd);
+int fd_is_valid(int fd);
+int send_start_message(local_id proc_numb, int*** matrix, int N);
+Message* create_message(char* buf, int length);
+
+typedef struct{
+	int*** fd_matrix;
+	local_id src;
+	int N;
+} SelfStruct;
 
 int main(int argc, char** argv){
 
-	pid_t pid;
 	int i, N;
-	int** fd_write = NULL;
-	int** fd_read = NULL;
+	pid_t pid;
 	local_id local_proc_id;
+	int*** fd_matrix;
+	int log_fd;
+	
+
+	if((log_fd = open(events_log, O_WRONLY)) == -1){
+		printf("Error: cannot open file %s.\n", events_log);
+		exit(1);
+	}
 
 	local_proc_id = PARENT_ID;
 
-	if((N = get_arg(argc, argv)) == -1){
-		exit(1);
-	}
-	
-	fd_write = create_matrix(N);
-	fd_read = create_matrix(N);
-
-	if((fill_fd_matrix(fd_write, fd_read, N)) == -1){
+	if((N = get_arg(argc,argv)) == -1){
 		exit(1);
 	}
 
-	for (i = 0; i < N; i++){
+	fd_matrix = create_matrix(N);
+	if(fill_matrix(fd_matrix, N) == -1){
+		exit(1);
+	}
+
+	for(i = 0; i < N; i++){
 		switch(pid = fork()){
 			case -1:
 				printf("Error\n");
-				break;
+				exit(1);
 			case 0:
 				printf("Child pid=%i ppid=%i\n",getpid(),getppid());
 				local_proc_id = i + 1;
-				close_unneccessary_fd(fd_write, N, local_proc_id);
-				close_unneccessary_fd(fd_read, N, local_proc_id);
+				if(close_unneccessary_fd(fd_matrix, N, local_proc_id) == -1){
+					exit(1);
+				}
+
+				send_start_message(local_proc_id, fd_matrix, N);
+
 
 				exit(0);
 			default:
@@ -59,103 +74,91 @@ int main(int argc, char** argv){
 		}
 	}
 
-	close_unneccessary_fd(fd_write, N, PARENT_ID);
-	close_unneccessary_fd(fd_read, N, PARENT_ID);
+	if(close_unneccessary_fd(fd_matrix, N, PARENT_ID) == -1){
+		exit(1);
+	}
 
 	for (i = 0; i < N; i++){
 		printf("Waiting...\n");
 		wait(&pid);
 	}
 
-	// int j;
-	// for(i = 0; i < N + 1; i++){
-	// 	for(j = 0; j < N + 1; j++){
-	// 		if(i == j){
-	// 			printf("x ");
-	// 		}
-	// 		else{
-	// 			printf("%i ", fd_write[i][j]);
-	// 		}
-
-	// 		// else if(fd_is_valid(fd[i][j])){
-	// 		// 	printf("+");
-	// 		// }else{
-	// 		// 	printf("-");
-	// 		// }
-	// 	}
-	// 	printf("\n");
-	// }
 
 	printf("End.\n");
 	return 0;
 }
 
-int proc_start(){
-
-	return 0;
+int fd_is_valid(int fd){
+	return fcntl(fd, F_GETFD) != -1;
 }
 
-int** create_matrix(int N){
-	int** fd;
-	int i;
-	fd = (int**)malloc((N + 1) * sizeof(int*));
-	for (i = 0; i <= N; i++){
-		fd[i] = (int*)malloc((N + 1) * sizeof(int));
-	}
-	return fd;
-}
-
-int create_pipe(int** fd, int i, int j, int pfd[2], int log_fd){
+int send_start_message(local_id proc_numb, int*** matrix, int N){
 	char buf[100];
+	Message* msg;
+	SelfStruct* selfstruct = (SelfStruct*)malloc(sizeof(SelfStruct));
 
-	if(pipe(pfd) == -1){
-		printf("Error: cannot create pipe.\n");
+	sprintf(buf, log_started_fmt,proc_numb, getpid(), getppid());
+	msg = create_message(buf, strlen(buf));
+	selfstruct->N = N;
+	selfstruct->fd_matrix = matrix;
+	selfstruct->src = proc_numb;
+	if(send_multicast(selfstruct, msg) == -1){
 		return -1;
 	}
-	sprintf(buf,"Pipe %i - %i created, fd0(r) = %i fd1(w) = %i\n", i, j, pfd[0], pfd[1]);
-	if(write(log_fd, buf, strlen(buf)) == -1){
-		printf("Error: cannot write to %s\n", pipes_log);
-		return -1;
-	}
-	fd[i][j] = pfd[1];
-	fd[j][i] = pfd[0];
 	return 0;
 }
 
-int fill_fd_matrix(int** fd_w, int** fd_r, int N){
-	int i, j;
-	int pipe_fd[2];
-	int log_fd;
-	
-	if((log_fd = open(pipes_log, O_WRONLY)) == -1){
-		printf("Error: cannot open file %s.\n", pipes_log);
+Message* create_message(char* buf, int length){
+	MessageHeader* header=(MessageHeader*)malloc(sizeof(MessageHeader));
+	Message* msg = (Message*)malloc(sizeof(Message));
+	header->s_magic = MESSAGE_MAGIC;
+	header->s_payload_len = length;
+	header->s_type = STARTED;
+	header->s_local_time = time(NULL);
+
+	msg->s_header = *header;
+	memcpy(msg->s_payload, buf, length);
+
+	return msg;
+}
+
+int send(void * self, local_id dst, const Message * msg){
+	SelfStruct* selfstruct = (SelfStruct*)self;
+	int fd;
+	local_id src;
+	int*** matrix;
+
+	src = selfstruct->src;
+	matrix = selfstruct->fd_matrix;
+	fd = matrix[src][dst][1];
+
+
+	if(!fd_is_valid(fd)){
+		printf("Error: invalid fd: %i\n", fd);
 		return -1;
 	}
+	if(write(fd, msg, sizeof(MessageHeader) + msg->s_header.s_payload_len) == -1){
+		printf("Error: cannot write to: %i\n", fd);
+		return -1;
+	}
+	return 0;
+}
 
-	for(i = 0; i < N + 1; i++){
-		for(j = i + 1; j < N + 1; j++){
-			if(create_pipe(fd_w, i, j, pipe_fd, log_fd) == -1){
-				return -1;
-			}
+int send_multicast(void * self, const Message * msg){
+	int i;
+	SelfStruct* selfstruct = (SelfStruct*)self;
+	local_id src;
+	int N;
 
-			if(create_pipe(fd_r, i, j, pipe_fd, log_fd) == -1){
-				return -1;
-			}
+	src = selfstruct->src;
+	N = selfstruct->N;
+
+	for(i = 0; i <= N; i++){
+		if(i == src){
+			continue;
 		}
-	}
-
-	swap_parts(fd_w, fd_r, N);
-	return 0;
-}
-
-int swap_parts(int** fd_w, int** fd_r, int N){
-	int i, j;
-	int t;
-	for(i = 0; i < N + 1; i++){
-		for(j = i + 1; j < N + 1; j++){
-			t = fd_w[j][i];
-			fd_w[j][i] = fd_r[i][j];
-			fd_r[i][j] = t;
+		if(send(self, i, msg) == -1){
+			return -1;
 		}
 	}
 	return 0;
@@ -163,37 +166,92 @@ int swap_parts(int** fd_w, int** fd_r, int N){
 
 int get_arg(int argc, char** argv){
 	int N;
-	if(argc < 3 || strcmp(argv[1],"-p") != 0 || (N = atoi(argv[2])) < 0){
+	if(argc!=3 || strcmp(argv[1],"-p") != 0 || (N = atoi(argv[2])) <= 0){
 		printf("Wrong arguments.\nUsage:\n\tpa1 [-p number] (number > 0)\n");
 		return -1;
 	}
 	return N;
 }
 
-int fd_is_valid(int fd){
-	return fcntl(fd, F_GETFD) != -1;
+int*** create_matrix(int N){
+	int i, j;
+	int *** matrix = NULL;
+
+	matrix = (int***)malloc((N + 1) * sizeof(int**));
+	for(i = 0; i <= N; i++){
+		matrix[i]=(int**)malloc((N + 1) * sizeof(int*));
+		for(j = 0; j <= N; j++){
+			matrix[i][j] = (int*)malloc(2 * sizeof(int));
+		}
+	}
+	return matrix;
+}
+
+int fill_matrix(int*** matrix, int N){
+	int i, j;
+	char buf[100];
+	int fd[2];
+	int log_fd;
+
+	if((log_fd = open(pipes_log, O_WRONLY)) == -1){
+		printf("Error: cannot open file %s.\n", pipes_log);
+		return -1;
+	}
+
+	for(i = 0; i <= N; i++){
+		for(j = 0; j <= N; j++){
+			if(i == j){
+				continue;
+			}
+			if(pipe(fd) == -1){
+				printf("Error: cannot create pipe.\n");
+				return -1;
+			}
+			matrix[i][j][0]=fd[0];
+			matrix[i][j][1]=fd[1];
+			sprintf(buf,"Pipe %i - %i created, fd0(r) = %i fd1(w) = %i\n", i, j, fd[0], fd[1]);
+			if(write(log_fd, buf, strlen(buf)) == -1){
+				printf("Error: cannot write to %s\n", pipes_log);
+				return -1;
+			}
+		}
+	}
+	return 0;
 }
 
 int close_fd(int fd){
 	if(close(fd) == -1){
-		printf("Error: cannot close pipe %i.\n", fd);	
+		printf("Error: cannot close pipe %i.\n", fd);
 		return -1;
 	}
 	return 0;
 }
 
-int close_unneccessary_fd(int** fd, int N, int local_proc_id){
-	int i, j;
-	for (i = 0; i < N + 1; i ++){
-		for (j = 0; j < N + 1; j ++){
+int close_unneccessary_fd(int*** matrix, int N, int proc_numb){
+	int i, j, k;
+
+	for(i = 0; i <= N; i++){
+		for(j = 0; j <= N; j++){
 			if(i == j){
 				continue;
 			}
-			if(i == local_proc_id || j == local_proc_id){
+			if(i == proc_numb){
+				if(close_fd(matrix[i][j][0]) == -1){
+					return -1;
+				}
 				continue;
 			}
-			
-			close_fd(fd[i][j]);
+			if(j == proc_numb){
+				if(close_fd(matrix[i][j][1]) == -1){
+					return -1;
+				}
+				continue;
+			}
+			for(k = 0; k < 2; k++){
+				if(close_fd(matrix[i][j][k]) == -1){
+					return -1;
+				}
+			}
 		}
 	}
 	return 0;
